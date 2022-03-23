953ddb158a8e37a3ba3557b6226ec30e2290d168
imzigma <eduardozigma@icloud.com>
2022-03-23 12:15:33 -0600

    first commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,44 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+const baseUrl = "https://platzi-avo.vercel.app";
+const mountNode = document.getElementById("js-mount");
 
-console.log('Happy hacking :)')
+const formatPrice = (price) =>
+  new Intl.NumberFormat("en-EN", {
+    style: "currency",
+    currency: "USD",
+  }).format(price);
+
+!(async function () {
+  const response = await fetch(`${baseUrl}/api/avo`);
+
+  const { data: allAvos } = await response.json();
+
+  const nodeArray = allAvos.map((avocado) => {
+
+    const image = document.createElement("img");
+    image.className =
+      "h-16 w-16 md:h-36 md:w-36 rounded-full mx-auto md:mx-0 md:mr-6 ";
+    image.src = `${baseUrl}${avocado.image}`;
+
+    const title = document.createElement("h2");
+    title.className = "text-lg font-bold text-gray-700";
+    title.textContent = avocado.name;
+
+    const price = document.createElement("div");
+    price.className = "text-gray-300 font-bold";
+    price.textContent = formatPrice(avocado.price);
+
+    const priceAndTitle = document.createElement("div");
+    priceAndTitle.className = "text-center md:text-left";
+    priceAndTitle.appendChild(title);
+    priceAndTitle.appendChild(price);
+
+    const card = document.createElement("div");
+    card.className = "md:flex p-6 items-center";
+    card.appendChild(image);
+    card.appendChild(priceAndTitle);
+
+    return card;
+  });
+
+  mountNode.append(...nodeArray);
+})();
